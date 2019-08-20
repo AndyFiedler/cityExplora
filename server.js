ccad11e08fd5c5607e0f2a2a9b20dfc68ad3e89d
Andy Fiedler <andyfidd@gmail.com>
2019-08-20 16:27:07 -0500

    andy changed request query from location to data

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(cors());
 //route for location
 app.get('/location', (request, response) => {
     try{
-        superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.location}&key=${process.env.GEOCODEAPI_KEY}`)
+        superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODEAPI_KEY}`)
         .then((geoData) => {
-            const location = new Location(request.query.location, geoData.body);
+            const location = new Location(request.query.data, geoData.body);
             response.send(location);
         });
         
